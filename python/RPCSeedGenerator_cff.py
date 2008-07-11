aa2cbaaefd5a2814265ae0ba1fd1743c12c5489b
cmsbuild <>
2008-07-11 14:34:55 +0000

    This commit was manufactured by cvs2git to create tag 'DQM-RPCMonitorDigi- CMSSW_2_1_0'.
    Sprout from master 2008-06-03 15:20:23 UTC Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch> 'eliminated debug couts'
    Cherrypick from master 2008-07-05 11:16:58 UTC Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch> 'Change "root" dependency to "rootgraphics"':
        BuildFile
        data/RPCSeedGenerator.cff
    Cherrypick from master 2008-07-11 14:34:54 UTC Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch> 'remove importing pythin fragments provided in Configuration.StandardSequences.Geoemtry_cff. The latter should be included in all high level configuration files':
        python/RPCSeedGenerator_cff.py

--- python/RPCSeedGenerator_cff.py
+++ python/RPCSeedGenerator_cff.py
@@ -1,13 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 # Magnetic Field
-from MagneticField.Engine.volumeBasedMagneticField_cfi import *
 # Geometry
 from Geometry.CMSCommonData.cmsIdealGeometryXML_cff import *
 from Geometry.CommonDetUnit.bareGlobalTrackingGeometry_cfi import *
-from Geometry.DTGeometry.dtGeometry_cfi import *
-from Geometry.CSCGeometry.cscGeometry_cfi import *
-from Geometry.RPCGeometry.rpcGeometry_cfi import *
 from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
 # Service
 from RecoMuon.TrackingTools.MuonServiceProxy_cff import *
