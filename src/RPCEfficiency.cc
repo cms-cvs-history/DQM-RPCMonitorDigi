d34ad83d3108d3713d76b4f849c104709d63e61f
cmsbuild <>
2010-01-26 15:39:21 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- RPCMonitorDigi-V03-03-19-01'.
    Sprout from master 2010-01-14 00:13:41 UTC Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch> '*** empty log message ***'
    Cherrypick from master 2010-01-26 15:39:19 UTC Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch> 'Adding protection for missgin collection':
        src/RPCEfficiency.cc

--- src/RPCEfficiency.cc
+++ src/RPCEfficiency.cc
@@ -289,8 +289,7 @@ void RPCEfficiency::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
     accessTorpcRecHits = false;
   }
 
-  if(accessTorpcRecHits){
-  
+  if(accessTorpcRecHits && rpcHits.isValid()){
   if(incldt){
     if(debug) std::cout<<"\t Getting the DT Segments"<<std::endl;
     edm::Handle<DTRecSegment4DCollection> all4DSegments;
@@ -303,7 +302,7 @@ void RPCEfficiency::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
       accessToDtSegments = false;
     }
 
-    if(accessToDtSegments){
+    if(accessToDtSegments && all4DSegments.isValid()){
       if(all4DSegments->size()>0){
 	if(all4DSegments->size()<=16) statistics->Fill(2);
 	
@@ -537,30 +536,38 @@ void RPCEfficiency::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
       }
     }
   }
-  
+    
   if(incldtMB4){
     if(debug) std::cout <<"MB4 \t Getting ALL the DT Segments"<<std::endl;
     edm::Handle<DTRecSegment4DCollection> all4DSegments;
     iEvent.getByLabel(dt4DSegments, all4DSegments);
     
+    bool accessToDtSegments = true;
+    try {
+      iEvent.getByLabel(dt4DSegments, all4DSegments);
+    }catch( cms::Exception& exception ) {
+      std::cout<<"DT Segments were not found"<<std::endl;
+      accessToDtSegments = false;
+    }
+    
+    if(accessToDtSegments && all4DSegments.isValid()){
     if(all4DSegments->size()>0){
-  
       std::map<DTChamberId,int> DTSegmentCounter;
       DTRecSegment4DCollection::const_iterator segment;  
-  
+      
       for (segment = all4DSegments->begin();segment!=all4DSegments->end(); ++segment){
 	DTSegmentCounter[segment->chamberId()]++;
       }    
-
+      
       if(debug) std::cout<<"MB4 \t \t Loop Over all4DSegments"<<std::endl;
       for (segment = all4DSegments->begin(); segment != all4DSegments->end(); ++segment){ 
-    
+	
 	DTChamberId DTId = segment->chamberId();
-
+	
 	if(debug) std::cout<<"MB4 \t \t This Segment is in Chamber id: "<<DTId<<std::endl;
 	if(debug) std::cout<<"MB4 \t \t Number of segments in this DT = "<<DTSegmentCounter[DTId]<<std::endl;
 	if(debug) std::cout<<"MB4 \t \t \t Is the only one in this DT? and is in the Station 4?"<<std::endl;
-
+	
 	if(DTSegmentCounter[DTId] == 1 && DTId.station()==4){
 
 	  if(debug) std::cout<<"MB4 \t \t \t yes"<<std::endl;
@@ -845,6 +852,8 @@ void RPCEfficiency::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
       if(debug) std::cout<<"MB4 \t This event doesn't have 4D Segment"<<std::endl;
     }
   }
+  }
+  
 
   if(inclcsc){
     if(debug) std::cout <<"\t Getting the CSC Segments"<<std::endl;
@@ -858,7 +867,7 @@ void RPCEfficiency::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
       accessToCSCSegments = false;
     }   
     
-    if(accessToCSCSegments){ 
+    if(accessToCSCSegments && allCSCSegments.isValid()){ 
       if(allCSCSegments->size()>0){
 	statistics->Fill(18);
 	
