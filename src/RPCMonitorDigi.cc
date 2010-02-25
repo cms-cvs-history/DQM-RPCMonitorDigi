9ce50a4d38f00052e8856036fe613e496d32e40b
cmsbuild <>
2010-02-25 14:46:24 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- RPCMonitorDigi-V03-03-19-06'.
    Sprout from master 2010-02-25 14:46:22 UTC Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch> 'Fixing // that crashes when running the whole sequence'
    Cherrypick from master 2010-01-14 00:13:41 UTC Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch> '*** empty log message ***':
        BuildFile
        src/SealModule.cc
    Cherrypick from DQM-RPCMonitorDigi-unlabeled-1.45.4 2010-02-17 17:07:07 UTC Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch> 'bug fixes':
        src/RPCBookDetUnitME.cc
    Cherrypick from DQM-RPCMonitorDigi-unlabeled-1.71.2 2010-02-17 17:07:08 UTC Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch> 'bug fixes':
        src/RPCMonitorDigi.cc
    Delete:
        BuildFile.xml
        test/rpcefficiency_cfg.py

--- src/RPCMonitorDigi.cc
+++ src/RPCMonitorDigi.cc
@@ -64,9 +64,9 @@ void RPCMonitorDigi::beginJob(){
   NumberOfClusters_for_EndcapPositive = dbe -> book1D("NumberOfClusters_for_EndcapPositive", "NumberOfClusters for Endcap Positive", 20, 0.5, 20.5);
   NumberOfClusters_for_EndcapNegative = dbe -> book1D("NumberOfClusters_for_EndcapNegative", "NumberOfClusters for Endcap Negative", 20, 0.5, 20.5);
   
-  NumberOfDigis_for_Barrel = dbe -> book1D("NumberOfDigi_for_Barrel", "Multiplicity in Barrel", 50, 0.5, 50.5);
-  NumberOfDigis_for_EndcapPositive = dbe -> book1D("NumberOfDigi_for_EndcapPositive", "Multiplicity in EndCapPositive", 50, 0.5, 50.5);
-  NumberOfDigis_for_EndcapNegative= dbe -> book1D("NumberOfDigi_for_EndcapNegative", "Multiplicity in EndCapNegative", 50, 0.5, 50.5);
+  NumberOfDigis_for_Barrel = dbe -> book1D("NumberOfDigi_for_Barrel", "Number Of Digis in Barrel", 50, 0.5, 50.5);
+  NumberOfDigis_for_EndcapPositive = dbe -> book1D("NumberOfDigi_for_EndcapPositive", "Number Of Digis in EndCapPositive", 50, 0.5, 50.5);
+  NumberOfDigis_for_EndcapNegative= dbe -> book1D("NumberOfDigi_for_EndcapNegative", "Number Of Digis in EndCapNegative", 50, 0.5, 50.5);
   
   SameBxDigisMeBarrel_ = dbe->book1D("SameBXDigis_Barrel", "Digis with same bx", 20, 0.5, 20.5);  
   SameBxDigisMeEndcapPositive_ = dbe->book1D("SameBXDigis_EndcapPositive", "Digis with same bx", 20, 0.5, 20.5);  
@@ -307,8 +307,8 @@ void RPCMonitorDigi::analyze(const Event& iEvent,const EventSetup& iSetup ){
     if(meMap[os.str()])   meMap[os.str()]->Fill(numberOfDigi);   
     
     if(detId.region()==0) NumberOfDigis_for_Barrel -> Fill(numberOfDigi);
-    else  if(detId.region()==-1) NumberOfDigis_for_EndcapPositive -> Fill(numberOfDigi);
-    else  if(detId.region()==1)  NumberOfDigis_for_EndcapNegative -> Fill(numberOfDigi);
+    else  if(detId.region()==1) NumberOfDigis_for_EndcapPositive -> Fill(numberOfDigi);
+    else  if(detId.region()==-1)  NumberOfDigis_for_EndcapNegative -> Fill(numberOfDigi);
                                 
     // Fill RecHit MEs   
     if(recHitCollection.first!=recHitCollection.second ){   
@@ -371,6 +371,7 @@ void RPCMonitorDigi::analyze(const Event& iEvent,const EventSetup& iSetup ){
       }/// end loop on RPCRecHits for given roll
       
       if(dqmexpert) {	 
+
 	os.str("");
 	os<<"NumberOfClusters_"<<nameRoll;
 	if(meMap[os.str()])
